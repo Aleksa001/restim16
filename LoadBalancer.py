187bd27325d0d6595765b3b3e7aa9159440c741e
Luka Zdravkovic <zdravkovicc.lukaa@gmail.com>
2022-06-04 15:24:31 -0700

    TCP connection LB and Writer

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -1,8 +1,27 @@
 from Podatak import Electricity
+import socket, json
 
-#lista u koju smestamo podatke tipa Podatak koje salje Writer komponenta
+# lista u koju smestamo podatke tipa Podatak koje salje Writer komponenta
 buffer = list(range(10))
 
+HOST = 'localhost'
+PORT = 50007
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+    try:
+        data_encoded = conn.recv(4096)
+        data_string = data_encoded.decode(encoding="utf-8")
 
-
+        data_variable = json.loads(data_string)
+        # data_variable is a dict representing your sent object
+        print(data_variable)
+    except:
+        #print(ConnectionAbortedError)
+        break
+    print('Data received from client')
+conn.close()
 
--- Writer.py
+++ Writer.py
@@ -1,4 +1,10 @@
 from Podatak import Electricity
+import socket, json
+HOST = 'localhost'
+PORT = 50007
+# Create a socket connection.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
 with open('StrujaFile.txt') as f:
     contents = f.readlines()
@@ -8,8 +14,19 @@ with open('StrujaFile.txt') as f:
     while 1 > 0:
         if i == count:
             break
-        one = Electricity(personal_id=contents[i], monthly_value=contents[i + 1], month=contents[i + 2])
-        print(one)
+        variable = Electricity(personal_id=contents[i], monthly_value=contents[i + 1], month=contents[i + 2])
+        # Map your object into dict
+        data_as_dict = vars(variable)
+
+        # Serialize your dict object
+        data_string = json.dumps(data_as_dict)
+
+        # Send this encoded object
+        s.send(data_string.encode(encoding="utf-8"))
+
+        #
+        print('Data Sent to Server')
         i = i+3
 
 
+s.close()
