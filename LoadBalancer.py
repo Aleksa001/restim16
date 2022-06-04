7677d2fd59dbf2d58a265a118b5be0131a7dd6fd
Aleksa001 <astokic20017@gmail.com>
2022-06-04 15:06:59 +0200

    Prepravka koda

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -1,7 +1,26 @@
 from Podatak import Electricity
 
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
 #lista u koju smestamo podatke tipa Podatak koje salje Writer komponenta
 buffer = list(range(10))
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(Electricity)
+            print(data)
+            if not data:
+                break
 
 
 
--- Writer.py
+++ Writer.py
@@ -1,5 +1,11 @@
 from Podatak import Electricity
 
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+
 with open('StrujaFile.txt') as f:
     contents = f.readlines()
     count = len(contents)
@@ -9,7 +15,11 @@ with open('StrujaFile.txt') as f:
         if i == count:
             break
         one = Electricity(personal_id=contents[i], monthly_value=contents[i + 1], month=contents[i + 2])
-        print(one)
+        #ovde salje LB-u podatke
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(one)
+
         i = i+3
 
 
