94eecff41e2880f86a2b3ad6be455fbccf5bf124
Aleksa001 <astokic20017@gmail.com>
2022-06-05 19:40:19 +0200

    Implementacija CRUD operacija.

--- DataBaseCRUD.py
+++ DataBaseCRUD.py
@@ -1,15 +1,48 @@
 import mysql.connector
 
+# DataBase CRUD ce koristiti ovu funkciju za manipulisanje bazom
+# F-ja vraca objekat mycur preko kog se izvrsavaju upiti za bazu podataka pomocu metode mycur.execute("Upit")
+db = mysql.connector.connect(
+    host="localhost",
+    user="Aleksa",
+    password="Ale.01Sto",
+    database="Projekat"
+)
+mycur = db.cursor()
 
 
-#DataBase CRUD ce koristiti ovu funkciju za manipulisanje bazom
-#F-ja vraca objekat mycur preko kog se izvrsavaju upiti za bazu podataka pomocu metode mycur.execute("Upit")
-def connection():
-    db = mysql.connector.connect(
-        host="localhost",
-        user="Aleksa",
-        password="Ale.01Sto",
-    )
-    mycur = db.cursor()
+def insertInDatabase(id, value, month):
+    try:
+        mycur.execute("INSERT INTO Potrosnjabrojila (IDBrojila, Potrosnja, Mesec) VALUES (%d, %f, '%s')" %(id,value,month))
+        db.commit()
+    except:
+        print("Error in operation!!!")
+        db.rollback()
 
-    return mycur
+def deletefromDatabase(id):
+    try:
+        mycur.execute("DELETE from Potrosnjabrojila where IDBrojila=%d" %(id))
+        db.commit()
+    except:
+        print("Error in operation!!!")
+        db.rollback()
+
+def updateInDatabase(id,value,month):
+    try:
+        mycur.execute("Update Potrosnjabrojila set Potrosnja= %f where IDBrojila= %d && Mesec='%s'" %(value,id,month))
+        db.commit()
+    except:
+        print("Error in operation!!!")
+        db.rollback()
+
+
+def readAllInDatabase():
+    mycur.execute("SELECT * FROM Potrosnjabrojila")
+    while True:
+        item=mycur.fetchone()
+        if item==None:
+            break
+        print(item)
+
+
+
